e6e69a9831e06513e386b750a57ca794b406fe54
Ha Pham <hpham2015@outlook.com>
2018-10-14 03:25:36 +0000

    barebones skeleton is up and running

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+  res.render("landing");
+});
+
+app.get("/customerInputForm", function(req, res){
+  res.render("customerInputForm");
+});
+
+app.get("/repairOrderForm", function(req, res){
+  res.render("repairOrderForm");
+});
+
+app.get("/vehicleInputForm", function(req, res){
+  res.render("vehicleInputForm");
+});
+
+app.get("/vehicleInspectionForm", function(req, res){
+  res.render("vehicleInspectionForm");
+});
+
+// Whoever is not on aws cloud 9, your ports will be different.
+app.listen(process.env.PORT, process.env.IP, function(){
+  console.log("Server started.");
+})

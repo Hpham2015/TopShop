fbec9ac45dbef94b271b26e17a7a600d3635a3ce
Ha Pham <hpham2015@outlook.com>
2018-10-26 00:18:55 +0000

    updated app.use for public

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){

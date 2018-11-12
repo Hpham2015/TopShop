19430a7b089c115a432f96f4669991ef934e40e2
EC2 Default User <ec2-user@ip-172-31-42-178.us-west-2.compute.internal>
2018-11-12 22:00:57 +0000

    Moved schemas for all objects into separate files in models folder, still need to work out dependencies between customer, vehicle, and last service schemas

---
+++ models/vehicleSchema.js
@@ -0,0 +1,20 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var lastServiceSchema = mongoose.model("LastServiceModel").schema;
+
+var vehicleSchema = new Schema({
+    customerID: Number,
+    make: String,
+    model: String,
+    year: Number,
+    licenseNum: Number,
+    color: String,
+    type: String,
+    productionDate: Date,
+    inserviceDate: Date,
+    lastSrvc: lastServiceSchema
+});
+
+module.exports = mongoose.model("VehicleModel", vehicleSchema);

e4101060ca9075f618822a5a9f940b0be5853146
EC2 Default User <ec2-user@ip-172-31-42-178.us-west-2.compute.internal>
2018-11-12 22:04:21 +0000

    removed schema initializations, added search functions that do not affect rest of file

--- app.js
+++ app.js
@@ -8,53 +8,24 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 })); 
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
-  
+
 // connects to MongoDB server
-mongoose.connect('mongodb://localhost:27017/TopShop', {useNewUrlParser: true});
+var mongoURL = 'mongodb://localhost:27017/myDB';
+mongoose.connect(mongoURL, {useNewUrlParser: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+require('/models');
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-// sets up schema to store last service info
-var lastServiceSchema = new mongoose.Schema({
-  date: Date,
-  odometer: Number,
-  dailyAverageMiles: Number,
-  monthlyAverageMiles: Number
-});
-
-// sets up vehicle entry form schema
-var vehicleSchema = new mongoose.Schema({
-  customerID: Number,
-  make: String,
-  model: String,
-  year: Number,
-  color: String,
-  type: String,
-  productionDate: Date,
-  inserviceDate: Date,
-  lastSrvc: lastServiceSchema
-});
-
-// sets up customer entry form schema
-var customerSchema = new mongoose.Schema({
-  customerID: Number,
-  firstName: String,
-  lastName: String,
-  address: String,
-  city: String,
-  state: String,
-  zip: Number,
-  email: String,
-  cell: String,
-  work: String,
-  vehicles: [vehicleSchema]
-});
-
-
-
 // initializes customer object model
-var customerObj = mongoose.model("customer", customerSchema);
+var customerObj = require('./models/customerSchema.js');
+// initializes vehicle and last service object models
+var vehicleObj = require('./models/vehicleSchema.js');
+var lastServiceObj = require('./models/lastServiceSchema.js');
 
 // adds new customer to DB
 app.post("/customerInputForm", function(req, res){
@@ -71,7 +42,13 @@ app.post("/customerInputForm", function(req, res){
     work: req.body.work
   });
   
-  console.log(newCustomerObj);
+  var vin = req.body.VIN;
+  var query = vehicleObj.findOne({VIN: vin}, function (err, vehicleObj) {
+    if (err) {
+      res.send(err);
+    }
+    console.log(vehicleObj);
+    newCustomerObj.vehicles.push(vehicleObj);
   
   newCustomerObj.save(function(err) {
     if (err) {
@@ -82,10 +59,6 @@ app.post("/customerInputForm", function(req, res){
 });
 
 });
-
-// initializes vehicle and last service object models
-var vehicleObj = mongoose.model("vehicle", vehicleSchema);
-var lastServiceObj = mongoose.model("lastService", lastServiceSchema);
 
 // adds new vehicle to DB
 app.post("/vehicleInputForm", function(req, res){
@@ -117,12 +90,26 @@ app.post("/vehicleInputForm", function(req, res){
 
 });
 
+// function to search for customers in database
+function searchCustomer(first , last, email) {
+  customerObj.find({firstName: first, lastName: last, email: email}, function (err, customer) {
+    if (err) return console.log("Could not find specified customer.");
+    else return customer;
+  });
+}
+
+// function to search for vehicles in database
+function searchVehicle(make, model, year, license) {
+  vehicleObj.find({make: make, model: model, year: year, licenseNum: license}, function (err, vehicle) {
+    if (err) return console.log("Could not find specified vehicle.");
+    else return vehicle;
+  });
+}
+
 // takes input from search form to search for items, then returns JSON with info to form
 app.post("/displaySearch", function(req, res) {
   
-  
 });
-
 
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
@@ -150,4 +137,6 @@ app.get("/vehicleInspectionForm", function(req, res){
 // Whoever is not on aws cloud 9, your ports will be different.
 app.listen(process.env.PORT, process.env.IP, function(){
   console.log("Server started.");
-})
+});
+
+});

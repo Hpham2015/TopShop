e21ca2bfad5ec1c0e56205164e013ad9f031f268
liorpeled <liorpeledw@gmail.com>
2018-11-14 21:12:44 -0800

    Add files via upload
    need to make sure the script file is at the same level as stylesheets

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,36 +1,61 @@
 window.setTimeout(function() {
   // your code here
 var NewCustomerButton = document.getElementById("NC");
-var RepairOrderButton = document.getElementById("RO");
-var NewVehicleButton = document.getElementById("NV");
+//var RepairOrderButton = document.getElementById("RO");
+//var NewVehicleButton = document.getElementById("NV");
 var VehicleInspectionButton = document.getElementById("VI");
 var logoHome = document.getElementById("logo");
+var ad1 = document.getElementById("firstAd");
+var ad2 = document.getElementById("secondAd");
+var ad3 = document.getElementById("thirdAd");
+var ad4 = document.getElementById("forthAd");
+var overlay=document.getElementsByClassName("overlay");
+
 
 NewCustomerButton.addEventListener("click",newCustomer);
-RepairOrderButton.addEventListener("click",repairOrder);
-NewVehicleButton.addEventListener("click",newVehicle);
+//RepairOrderButton.addEventListener("click",repairOrder);
+//NewVehicleButton.addEventListener("click",newVehicle);
 VehicleInspectionButton.addEventListener("click",vehicleInspection);
+var i=0;
 
-
-
-
-
-
+setInterval(function(){
+     adChange(i);
+     i++;
+     if(i==4)
+     {
+         i=0;
+     }
+     
+     console.log("got loop every three secondss");
+},9500);       
 
 
 function newCustomer(){location.href = "/customerInputForm";}
-function repairOrder(){location.href = "/repairOrderForm";}    
-function newVehicle(){location.href = "/vehicleInputForm";}    
-function vehicleInspection(){location.href = "/vehicleInspectionForm";}   
+//function repairOrder(){location.href = "/repairOrderForm";}    
+//function newVehicle(){location.href = "/vehicleInputForm";}    
+function vehicleInspection(){location.href = "/dashboard";}   
 
-
-
-/*function newCustomerHighLight(){location.href = "/customerInputForm";}
-function repairOrderHighLight(){location.href = "/repairOrderForm";}    
-function newVehicleHighLight(){location.href = "/vehicleInputForm";}    
-function vehicleInspectionHighLight(){location.href = "/vehicleInspectionForm";} */  
-
-
+ 
+function adChange(element)
+{
+    
+var headerElements=document.getElementsByTagName("h1");
+var pElement = headerElements[element];
+     console.log(pElement);
+     //overlay.style.right=Math.floor(Math.random() * 101) + 50;
+     pElement.setAttribute("class", "fadeIn");
+    
+     setTimeout(function() {
+            
+         pElement.setAttribute("class","fadeOut");
+      
+     }, 5000);
+            
+           
+      
+    
+    
+}
 
 
 

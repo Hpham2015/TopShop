e8e4f052f243573a1d2eabedd33a5532f35b062f
s2eagle <jhui7@ucsc.edu>
2018-11-28 01:49:27 +0000

    Fixed VIN being a number in schemas. Dummy data now has more real values.

--- populateDatabase.js
+++ populateDatabase.js
@@ -8,76 +8,76 @@ var repairOrderModel = require('./models/RepairOrderFormSchema.js');
 module.exports = {
     populate() {
       var lastService1 = new lastServiceModel({
-        date: new Date('December 1, 1111 01:11:11'),
+        date: new Date('December 1, 2011 01:11:11'),
         odometer: 11111,
-        dailyAverageMiles: 11111,
-        monthlyAverageMiles: 11111
+        dailyAverageMiles: 30,
+        monthlyAverageMiles: 900
       });
       
       var lastService2 = new lastServiceModel({
-        date: new Date('December 2, 2222 02:22:22'),
+        date: new Date('December 2, 2012 02:22:22'),
         odometer: 22222,
-        dailyAverageMiles: 22222,
-        monthlyAverageMiles: 22222
+        dailyAverageMiles: 1,
+        monthlyAverageMiles: 30
       });
       
       var lastService3 = new lastServiceModel({
-        date: new Date('December 3, 3333 03:33:33'),
+        date: new Date('December 3, 2013 03:33:33'),
         odometer: 33333,
-        dailyAverageMiles: 33333,
-        monthlyAverageMiles: 33333
+        dailyAverageMiles: 50,
+        monthlyAverageMiles: 15000
       });
       
       var newVehicle1 = new vehicleModel({
-        customerID: 1111111111,
-        make: "make11111",
-        model: "model11111",
-        year: 1111,
-        licenseNum: "License1",
-        VIN: 111,
-        color: "color111",
-        type: "type1111",
+        customerID: 1672548348,
+        make: "Toyota",
+        model: "Camry",
+        year: 2019,
+        licenseNum: "9J1JB12",
+        VIN: "1FAPP36X6RK192113",
+        color: "Red",
+        type: "Midsize",
         mileage: 11111,
         lastSrvc: lastService1
       });
       
       var newVehicle2 = new vehicleModel({
-        customerID: 2222222222,
-        make: "make22222",
-        model: "model22222",
-        year: 2222,
-        licenseNum: "License2",
-        VIN: 222,
-        color: "color222",
-        type: "type2222",
+        customerID: 2924637168,
+        make: "Honda",
+        model: "HR-V",
+        year: 2019,
+        licenseNum: "9H1LLF7",
+        VIN: "1C3CDZAG5DN749746",
+        color: "Red",
+        type: "Van",
         mileage: 22222,
         lastSrvc: lastService2
       });
       
       var newVehicle3 = new vehicleModel({
-        customerID: 3333333333,
-        make: "make33333",
-        model: "model33333",
-        year: 3333,
-        licenseNum: "License3",
-        VIN: 333,
-        color: "color333",
-        type: "type3333",
+        customerID: 3673157365,
+        make: "Acura",
+        model: "TLX",
+        year: 2019,
+        licenseNum: "9JWJA14",
+        VIN: "1N4AL3AP7FC188988",
+        color: "Red",
+        type: "Car",
         mileage: 33333,
         lastSrvc: lastService3
       });
       
       var newCustomer1 = new customerModel({
-        customerID: 1111111111,
+        customerID: 1672548348,
         firstName: "John", 
         lastName: "Connor",
         street: "111 Nonya Business",
         city: "New York",
         state: "NY",
-        zip: 11111,
-        email: "11111@youdieded.com",
-        cell: 1231111111,
-        work: 1111111111,
+        zip: 17315,
+        email: "John@private.com",
+        cell: 123164438,
+        work: 647321486,
         vehicles: [
           newVehicle1,
           newVehicle2,
@@ -86,16 +86,16 @@ module.exports = {
       });
       
       var newCustomer2 = new customerModel({
-        customerID: 2222222222,
+        customerID: 2924637168,
         firstName: "John", 
         lastName: "Connor",
         street: "22222 Nonya Business",
         city: "New York",
         state: "NY",
-        zip: 22222,
-        email: "222222222@youdieded.com",
-        cell: 2222222222,
-        work: 2222222222,
+        zip: 24731,
+        email: "ConnorJ15@gmail.com",
+        cell: 2614147615,
+        work: 2653565643,
         vehicles: [
           newVehicle1,
           newVehicle2
@@ -103,16 +103,16 @@ module.exports = {
       });
       
       var newCustomer3 = new customerModel({
-        customerID: 3333333333,
-        firstName: "John", 
-        lastName: "Conner",
+        customerID: 3673157365,
+        firstName: "Jonathan", 
+        lastName: "Brown",
         street: "3333333 Nonya Business",
         city: "New York",
         state: "NY",
-        zip: 3333333,
-        email: "johnwick@youdieded.com",
-        cell: 3333333333,
-        work: 3333333333,
+        zip: 33333,
+        email: "BrownJ4076@gmail.com",
+        cell: 3948648961,
+        work: 3645564434,
         vehicles: [
           newVehicle1
         ]
@@ -123,7 +123,7 @@ module.exports = {
         complaint: "Complaint11",
         cause: "Cause111",
         resolution: "Resolution1111",
-        cost: "Cost11111"
+        cost: "$111.25"
       });
       
       var jobs2 = new jobModel({
@@ -131,7 +131,7 @@ module.exports = {
         complaint: "Complaint22",
         cause: "Cause222",
         resolution: "Resolution2222",
-        cost: "Cost22222"
+        cost: "$222.15"
       });
       
       var jobs3 = new jobModel({
@@ -139,76 +139,76 @@ module.exports = {
         complaint: "Complaint33",
         cause: "Cause333",
         resolution: "Resolution3333",
-        cost: "Cost33333"
+        cost: "$333.42"
       });
       
       var vehicleInspecion1 = new VehicleInspectionFormModel({
-        Name: "Name111",
-        Mileage: 111,
-        Year_Make_Model: "Yearmakemodel111",
-        VIN: 111,
-        License: "License1",
-        email: "email1",
-        Comments: "1car comment",
+        Name: "John Connor",
+        Mileage: 13715,
+        Year_Make_Model: "2019 Toyota Camry",
+        VIN: "1FAPP36X6RK192113",
+        License: "9J1JB12",
+        email: "John@private.com",
+        Comments: "Oil change needed",
         Inspected_on: new Date('December 1, 1111 01:11:11')
       });
       
       var vehicleInspecion2 = new VehicleInspectionFormModel({
-        Name: "Name222",
-        Mileage: 222,
-        Year_Make_Model: "Yearmakemodel222",
-        VIN: 222,
-        License: "License2",
-        email: "email2",
-        Comments: "2car comment",
+        Name: "John Connor",
+        Mileage: 27314,
+        Year_Make_Model: "2019 Honda HR-V",
+        VIN: "1C3CDZAG5DN749746",
+        License: "9H1LLF7",
+        email: "ConnorJ15@gmail.com",
+        Comments: "Broken Windshield",
         Inspected_on: new Date('December 2, 2222 02:22:22')
       });
       
       var vehicleInspecion3 = new VehicleInspectionFormModel({
-        Name: "Name333",
-        Mileage: 333,
-        Year_Make_Model: "Yearmakemodel333",
-        VIN: 333,
-        License: "License3",
-        email: "email3",
-        Comments: "3car comment",
+        Name: "Jonathan Brown",
+        Mileage: 37473,
+        Year_Make_Model: "2018 Acura TLX",
+        VIN: "1N4AL3AP7FC188988",
+        License: "9JWJA14",
+        email: "BrownJ4076@gmail.com",
+        Comments: "Everything looks good.",
         Inspected_on: new Date('December 3, 3333 03:33:33')
       });
     
       var repairOrder1 = new repairOrderModel({
         repairOrderNumber: "1",
-        customerID: "1111111111",
-        VIN: "111",
+        customerID: "1672548348",
+        VIN: "1FAPP36X6RK192113",
         inspectionReport: vehicleInspecion1,
-        mechanicID: "11111",
-        mechanicFirstName: "firstname111",
-        mechanicLastName: "lastname111",
+        mechanicID: "11874",
+        mechanicFirstName: "Tressa",
+        mechanicLastName: "Manthey",
         jobs: jobs1,
-        totalCost: "111"
+        totalCost: "$111.25"
       });
       
       var repairOrder2 = new repairOrderModel({
         repairOrderNumber: "2",
-        customerID: "2222222222",
-        VIN: "222",
+        customerID: "2924637168",
+        VIN: "1C3CDZAG5DN749746",
         inspectionReport: vehicleInspecion2,
-        mechanicID: "22222",
-        mechanicFirstName: "firstname222",
-        mechanicLastName: "lastname222",
+        mechanicID: "22731",
+        mechanicFirstName: "Cleta",
+        mechanicLastName: "Isenberg",
         jobs: jobs2,
-        totalCost: "222"
+        totalCost: "$222.15"
       });
       
       var repairOrder3 = new repairOrderModel({
         repairOrderNumber: "3",
-        customerID: "3333333333",
-        VIN: "111",
+        customerID: "3673157365",
+        VIN: "1N4AL3AP7FC188988",
         inspectionReport: vehicleInspecion3,
-        mechanicID: "33333",
-        mechanicFirstName: "firstname333",
-        mechanicLastName: "lastname333",
+        mechanicID: "33751",
+        mechanicFirstName: "Jacques",
+        mechanicLastName: "Cryer",
         jobs: jobs3,
-        totalCost: "333"
+        totalCost: "$333.42"
       });
       
       newCustomer1.save(function(error) {
